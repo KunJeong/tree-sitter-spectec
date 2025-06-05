8819f93a1abdba43c188b0363d75caea97a1ed53
KunJeong <kunjeong99@naver.com>
2025-06-06 00:22:39 +0900

    fix: updated broken queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -3,8 +3,8 @@
 [ "syntax" "var" "relation" "rule" "dec" "def" ] @keyword
 
 (comment) @comment
-(type_identifier) @type
-(int_literal) @number
-(function_signature_definition name: (identifier) @function)
-(function_definition name: (identifier) @function)
+; (_type) @type
+; (int_literal) @number
+(function_signature_definition name: (function_id) @function)
+(function_definition name: (function_id) @function)
 (constructor_id) @constructor
